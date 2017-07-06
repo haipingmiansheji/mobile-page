080fed90a962b71f570c8e0994582fbb9127c939
haipingmiansheji <240173221@qq.com>
2017-07-06 10:07:15 +0800

    手机页面

---
+++ js/js.js
@@ -0,0 +1,21 @@
+$(function(){
+	//扫描
+	(function(){
+		$('#scanning').bind('touchstart',function(){
+			alert('a')
+		})
+	})();
+	//消息
+	(function(){
+		$('#message').bind('touchstart',function(){
+			alert('a')
+		})
+	})();
+	//轮播
+	(function(){
+		
+	})();
+
+
+});
+
